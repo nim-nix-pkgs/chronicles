5d471006ac8d967893e2889c9a814f2681ca2a13
hugosenari <hugosenari@gmail.com>
2022-03-22 00:26:12 -0300

    chore: re-index ../nimpkgs/c/chronicles/v0_4_2

--- v0_4_2/flake.nix
+++ v0_4_2/flake.nix
@@ -9,7 +9,7 @@
   
   inputs.src-chronicles-v0_4_2.flake = false;
   inputs.src-chronicles-v0_4_2.owner = "status-im";
-  inputs.src-chronicles-v0_4_2.ref   = "refs/tags/v0.4.2";
+  inputs.src-chronicles-v0_4_2.ref   = "v0_4_2";
   inputs.src-chronicles-v0_4_2.repo  = "nim-chronicles";
   inputs.src-chronicles-v0_4_2.type  = "github";
   
